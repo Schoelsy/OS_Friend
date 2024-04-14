9a39c5008448fcbd5a4591ba678757f87c5fc110
Schoelsy <schoelsy@wp.pl>
2024-04-14 02:28:05 +0200

    Added tracing for better logs

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,26 @@ use std::path::PathBuf;
 
 use eyre::Result;
 use clap::Parser;
+use tracing::info;
 
 
 #[derive(Parser)]
 struct Cli {
     #[arg(short, long)]
-    movie_path: PathBuf,
+    pub movie_path: PathBuf,
     #[arg(short, long, default_value = "eng")]
-    language: String
+    pub language: String
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-
+    tracing_subscriber::fmt::init();
     let Cli {
         movie_path,
         language,
     } = Cli::parse();
 
-    println!("path to movie: {:?}, language: {:?} ", movie_path, language);
+   // println!("path to movie: {:?}, language: {:?} ", movie_path, language);
+    info!(?movie_path, language, "got");
     Ok(())
 }
