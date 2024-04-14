85d6f26b4cc6526b7e04c0e2a9e440bf1d75d5ae
Schoelsy <schoelsy@wp.pl>
2024-04-14 02:13:34 +0200

    Added tokio for async, eyre for result and clap for Cli parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use eyre::Result;
+use clap::Parser;
+
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long)]
+    movie_path: PathBuf,
+    #[arg(short, long, default_value = "eng")]
+    language: String
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    let Cli {
+        movie_path,
+        language,
+    } = Cli::parse();
+
+    println!("path to movie: {:?}, language: {:?} ", movie_path, language);
+    Ok(())
 }
